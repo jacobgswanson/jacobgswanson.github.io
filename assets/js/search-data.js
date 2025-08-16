922b789af8d6778b551bc296dabffdb4d0fcc4bd
jacobgswanson <101067496+jacobgswanson@users.noreply.github.com>
2025-08-16 13:48:09 +0000

    Deploying to gh-pages from @ jacobgswanson/jacobgswanson.github.io@3a20dc483a9cd9553016a7917c320801fe246168 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -421,9 +421,9 @@ ninja.data = [{
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-finance-amp-mass-incarceration",
+          title: 'Finance &amp;amp; Mass Incarceration',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
