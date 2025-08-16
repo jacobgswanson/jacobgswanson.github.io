04d12544d14bbd10eb55f2a3d159954c817d0e08
jacobgswanson <101067496+jacobgswanson@users.noreply.github.com>
2025-08-16 12:17:51 +0000

    Deploying to gh-pages from @ jacobgswanson/jacobgswanson.github.io@2c035ee18a6246933193d7c31472ca536969374c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -431,21 +431,11 @@ ninja.data = [{
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
             },},{id: "projects-project-7",
           title: 'project 7',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
             },},{id: "projects-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
